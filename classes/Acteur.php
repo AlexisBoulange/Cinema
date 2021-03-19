98a5f195eaaad534c4732827044bf2b2484b3b80
ZiCkYS <alexisboulange88@gmail.com>
2021-03-19 02:14:18 +0100

    Update Acteur.php

--- classes/Acteur.php
+++ classes/Acteur.php
@@ -1,161 +1,27 @@
 <?php
 
-class Acteur{
+class Acteur extends Individu{
 
-
-    private $id;
-    private $prenom;
-    private $nom;
-    private $sexe;
-    private $dateNaissance;
-    private $role;
-
-    public function __construct($id, $prenom, $nom, $sexe, $dateNaissance, $role){
-
-        $this->id = $id;
-        $this->prenom = $prenom;
-        $this->nom = $nom;
-        $this->sexe = $sexe;
-        $this->dateNaissance = $dateNaissance;
-        $this->role = $role;
-        
-    }
-
-    /**
-     * Get the value of id
-     */ 
-    public function getId()
+    private $casting =[];
+    
+    public function __toString()
     {
-        return $this->id;
+        return 'Acteur : '. parent::__toString();
     }
 
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
-    public function setId($id)
-    {
-        $this->id = $id;
-
-        return $this;
+    public function ajouterCasting(Role $role, Film $film){
+        array_push($this->castings, [$role, $film]);
     }
 
-    /**
-     * Get the value of prenom
-     */ 
-    public function getPrenom()
-    {
-        return $this->prenom;
+    public function afficherRoles(){
+        $str = $this->prenom . ' ' . $this-> nom . ' a joué dans : <br>';
+        foreach ($this->castings as $casting){
+            $str .= $casting[0]->getPersonnage() . ' - film : ' . $casting[1]->getTitre(). '<br>';
+        }
+        return $str;
     }
 
-    /**
-     * Set the value of prenom
-     *
-     * @return  self
-     */ 
-    public function setPrenom($prenom)
-    {
-        $this->prenom = $prenom;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of nom
-     */ 
-    public function getNom()
-    {
-        return $this->nom;
-    }
-
-    /**
-     * Set the value of nom
-     *
-     * @return  self
-     */ 
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of sexe
-     */ 
-    public function getSexe()
-    {
-        return $this->sexe;
-    }
-
-    /**
-     * Set the value of sexe
-     *
-     * @return  self
-     */ 
-    public function setSexe($sexe)
-    {
-        $this->sexe = $sexe;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of dateNaissance
-     */ 
-    public function getDateNaissance()
-    {
-        return $this->dateNaissance;
-    }
-
-    /**
-     * Set the value of dateNaissance
-     *
-     * @return  self
-     */ 
-    public function setDateNaissance($dateNaissance)
-    {
-        $this->dateNaissance = $dateNaissance;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of role
-     */ 
-    public function getRole()
-    {
-        return $this->role;
-    }
-
-    /**
-     * Set the value of role
-     *
-     * @return  self
-     */ 
-    public function setRole($role)
-    {
-        $this->role = $role;
-
-        return $this;
-    }
-
-    public function getAge(){
-        $date1 = new DateTime ("now");
-        $date2 = new DateTime ($this->getDateNaissance());
-        $interval = $date1->diff($date2);
-        $age = $interval->format('%y');
-        return $age;
-        
-    }
-
-    public function __toString(){
-        return $this->id.
-            "<br/> Prenom : ".$this->prenom.
-            "<br/> Nom : ".$this->nom.
-            "<br/> Sexe : ".$this->sexe.
-            "<br/> Date de naissance : ".$this->dateNaissance.
-            "<br/> Rôle : ".$this->role;
+    public function getNom(){
+        return $this->prenom . ' ' . $this->nom;
     }
 }
