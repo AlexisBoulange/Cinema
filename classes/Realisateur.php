e2a85c9e686d2e8591599039804a9d4c26e0df93
ZiCkYS <alexisboulange88@gmail.com>
2021-03-22 21:21:23 +0100

    Update Realisateur.php

--- classes/Realisateur.php
+++ classes/Realisateur.php
@@ -1,9 +1,25 @@
 <?php
 
 class Realisateur extends Individu{
+
+    private $films = [];
+
     public function __toString()
     {
         return 'Réalisateur : '. parent::__toString();
     }
 
+    public function ajouterFilm(Film $film){
+        array_push($this->film, [$film]);
+    }
+
+    public function getRealisations(){
+        $str = "Le réalisateur ". $this->prenom .' '. $this->nom. ' a réalisé les films suivants : ';
+        $str .= '<ul>';
+        foreach($this->films as $film){
+            $str .= '<li>'. $film[1]->getTitre().'</li>';    
+        }
+        $str .=  '</ul>';
+        return $str;
+    }
 }
