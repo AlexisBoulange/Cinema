4c580f48d42e0d95aae6f6ac42be578cb6fd0c0b
ZiCkYS <alexisboulange88@gmail.com>
2021-03-31 17:01:41 +0200

    Create detailReal.php

---
+++ views/realisateur/detailReal.php
@@ -0,0 +1,41 @@
+<?php
+
+
+ob_start();
+
+$detailReal = $realisateur->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+<h2>Détails de <?=$detailReal['realisateur']?></h2>
+
+
+<table>
+    <thead>
+        <tr>
+            <th>SEXE</th>
+            <th>DATE DE NAISSANCE</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+                echo "<tr><td>". $detailReal["sexe"]."</td>";
+                echo "<td>". $detailReal["date_naissance"]."</td>";
+                echo "<td>";
+                while($filmoR = $filmoRea->fetch()){
+                    echo "<a href='index.php?action=detailFilm&id= ". $filmoR["film_id"]."'</a>".$filmoR["titre"]."</br>";
+                }"</td></tr>";
+        ?>
+    </tbody>
+</table>
+
+
+
+
+
+<?php
+
+$realisateur->closeCursor();
+$titre = "Détail du réalisateur ".$detailReal['realisateur'];
+$contenu = ob_get_clean();
+require "views/template.php";
