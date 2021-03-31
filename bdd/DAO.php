4f7153ec5a1ee2201d0d072be1c7a6aeabf8c318
ZiCkYS <alexisboulange88@gmail.com>
2021-03-31 16:59:32 +0200

    Create DAO.php

---
+++ bdd/DAO.php
@@ -0,0 +1,37 @@
+<?php
+
+class DAO{
+
+    private $bdd;
+
+
+    public function __construct(){
+        $this->bdd = new PDO(
+            'mysql:host=localhost:3306;dbname=cinema',
+            'root',
+            ''
+        );
+    }
+    
+
+    /**
+     * Get the value of bdd
+     */ 
+    public function getBdd()
+    {
+        return $this->bdd;
+    }
+
+    public function executerRequete($sql, $params = NULL){
+        
+        if ($params == NULL){
+
+            $result = $this->bdd->query($sql);
+        }else{
+            $result = $this->bdd->prepare($sql);
+            $result->execute($params);
+        }
+
+        return $result;
+    }
+}
