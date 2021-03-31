ef94da43673ee679b3cc772fb8b8660d531d3bf9
ZiCkYS <alexisboulange88@gmail.com>
2021-03-31 16:59:51 +0200

    Update Role.php

--- classes/Role.php
+++ classes/Role.php
@@ -1,82 +1,52 @@
 <?php
 
-class Role{
-
+class Role
+{
     private $id;
     private $personnage;
     private $castings = [];
 
-    public function __construct($id, $personnage){
-
+    public function __construct($id, $personnage)
+    {
         $this->id = $id;
         $this->personnage = $personnage;
-        
     }
 
-    public function afficherRole(){
-            $str = "Le rôle ". $this->personnage .' a été interpreté par : ';
-            $str .= '<ul>';
-            foreach($this->castings as $casting){
-                $str .= '<li>'. $casting[1]->getTitre().' - Acteur :'. $casting[0]->getNom(). '</li>';    
-            }
-            $str .=  '</ul>';
-            return $str;
-    }
-
-    /**
-     * Get the value of id
-     */ 
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
-    public function setId($id)
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of personnage
-     */ 
-    public function getPersonnage()
+    public function __toString()
     {
         return $this->personnage;
     }
 
-    /**
-     * Set the value of personnage
-     *
-     * @return  self
-     */ 
-    public function setPersonnage($personnage)
+    public function ajouterCasting(Acteur $acteur, Film $film)
     {
-        $this->personnage = $personnage;
-
-        return $this;
-    }
-
-
-    public function __toString(){
-        return "<br/> Rôle : ".$this->personnage;
-    }
-    
-    public function ajouterCasting(Acteur $acteur, Film $film){
         array_push($this->castings, [$acteur, $film]);
+    }
+
+    public function afficherRole()
+    {
+        $str = "Le rôle " . $this->personnage . " a été interprété par:";
+        $str .= '<ul>';
+        foreach ($this->castings as $casting) {
+            // index 0 = acteur et index 1 = film
+            $str .= '<li>Acteur: ' . $casting[0]->getNom() . ' - Film: ' . $casting[1]->getTitre() . '</li>';
+        }
+        $str .= '</ul>';
+        return $str;
     }
 
     /**
      * Get the value of castings
-     */ 
+     */
     public function getCastings()
     {
-        return $this->castings;
+        return var_dump($this->castings);
+    }
+
+    /**
+     * Get the value of personnage
+     */
+    public function getPersonnage()
+    {
+        return $this->personnage;
     }
 }
