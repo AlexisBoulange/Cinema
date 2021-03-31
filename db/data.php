0c741dd46cad5f1a03a90668b3aa53cc8f47ff85
ZiCkYS <alexisboulange88@gmail.com>
2021-03-31 17:00:13 +0200

    Update data.php

---
+++ db/data.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'classes/Individu.class.php';
+require_once 'classes/Acteur.class.php';
+require_once 'classes/Realisateur.class.php';
+require_once 'classes/Film.class.php';
+require_once 'classes/Genre.class.php';
+require_once 'classes/Role.class.php';
+require_once 'classes/Casting.class.php';
