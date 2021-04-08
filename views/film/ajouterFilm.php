d066a58c03edfd2de4825fbdcc6ae7b830fda54d
ZiCkYS <alexisboulange88@gmail.com>
2021-04-08 20:50:12 +0200

    Update ajouterFilm.php

--- views/film/ajouterFilm.php
+++ views/film/ajouterFilm.php
@@ -7,7 +7,7 @@ ob_start();
 <a href="./index.php">Retour</a>
 
 
-<h2><?= $_POST[('prenom_du_realisateur')]." ".$_POST[('nom_du_realisateur')]?> a été rajouté à la liste de film avec succès</h2>
+<h2><?= $_POST[('titre')]?> a été rajouté à la liste de film avec succès</h2>
 
 
 
