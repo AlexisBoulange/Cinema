89f091813c80654bd47620b859c83cb9906f1752
ZiCkYS <alexisboulange88@gmail.com>
2021-04-08 20:50:03 +0200

    Update ajouterActeur.php

--- views/acteur/ajouterActeur.php
+++ views/acteur/ajouterActeur.php
@@ -7,7 +7,7 @@ ob_start();
 <a href="./index.php">Retour</a>
 
 
-<h2>Réalisateur <?= $_POST[('prenom_acteur')]." ".$_POST[('nom_acteur')]?> rajouté avec succès</h2>
+<h2>Acteur <?= $_POST[('prenom_acteur')]." ".$_POST[('nom_acteur')]?> rajouté avec succès</h2>
 
 
 
